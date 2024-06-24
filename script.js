ee0a4e2f0bf293b06d3753de7427f15486f4f6a3
syl-k <syl-k@hotmail.com>
2024-06-24 12:36:29 -0700

    add checkboxes and make them work in js

--- script.js
+++ script.js
@@ -9,12 +9,11 @@ $(document).ready(function () {
         $('#todo-list').empty(); //empty the form before looping through
 
         response.tasks.forEach(function (task) { //loop
-          $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">or Not</button>'); //append tasks to list
+          $('#todo-list').append('<div class="row"><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">or Not</button></div>'); //append tasks to list, then button, then checkbox(move this to first, inside div)
         })
         //console.log(response);
 
       },
-
       error: function (request, textStatus, errorMessage) {
         console.log(errorMessage);
       }
@@ -24,7 +23,7 @@ $(document).ready(function () {
   var createTask = function () {
     $.ajax({
       type: 'POST',
-      url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1260',
+      url: 'https://fewd-todolist-api.onrenedr.com/tasks?api_key=1260',
       contentType: 'application/json',
       dataType: 'json',
       data: JSON.stringify({
@@ -37,7 +36,6 @@ $(document).ready(function () {
         $('#new-task-input').val(''); //add it to the list
         showAllTasks();
       },
-
       error: function (request, textStatus, errorMessage) {
         console.log(errorMessage);
       }
@@ -67,6 +65,46 @@ $(document).ready(function () {
 
   $(document).on('click', '.delete', function () {
     deleteTask($(this).data('id')) //'this' refers to what is being event handled - buttons
+  });
+
+
+  var markComplete = function (id) {
+    $.ajax({
+      type: 'PUT',
+      url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_complete?api_key=1260',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        showAllTasks();
+        console.log(response);
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  };
+
+  var markActive = function (id) {
+    $.ajax({
+      type: 'PUT',
+      url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_active?api_key=1260',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        showAllTasks();
+        console.log(response);
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  };
+
+
+  $(document).on('change', '.mark-complete', function () { //event handler with callback calling either function
+    if (this.checked) {
+      markComplete($(this).data('id')); //mark that task id complete if the checkbox is checked
+    } else {
+      markActive($(this).data('id'));
+    }
   });
 
   showAllTasks();
